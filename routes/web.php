205ab16338409ba8370016be130cbe43688a9e52
Manuel Moreno <manuel.moreno@televisatim.com>
2020-08-31 13:01:50 -0500

    ADD:Test coneccion

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Database\Connection;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/coneccion', function () {
+    try {
+        \Illuminate\Support\Facades\DB::connection()->getPdo();
+        echo "Connected successfully to: " . DB::connection()->getDatabaseName();
+    }catch (\Exception $e){
+        die("Could not connect to the database. Please check your configuration. error:" . $e );
+    }
+
     return view('welcome');
 });
